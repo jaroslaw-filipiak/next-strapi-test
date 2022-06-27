f586f7e52f4476436748f1ca2b3c661b5db1c375
Jarosław Filipiak <info@j-filipiak.pl>
2022-06-27 18:45:29 +0200

    advertising privacy policy page

--- pages/advertising-privacy-policy.js
+++ pages/advertising-privacy-policy.js
@@ -1,19 +1,58 @@
 import Layout from '../components/layout';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_ADVERTISING_PRIVACY_POLICY_PAGE_DATA = gql`
+  query getFeatures {
+    pagePrivacyPolicyAdvertising(locale: "pl") {
+      data {
+        attributes {
+          page_title
+          page_content
+        }
+      }
+    }
+  }
+`;
 
 const advertisingPrivacyPolicy = () => {
+  const { data, error, loading } = useQuery(
+    GET_ADVERTISING_PRIVACY_POLICY_PAGE_DATA
+  );
+
+  if (loading) return <p>loading..</p>;
+  if (error) return <p>error...</p>;
+
+  console.log(data);
+
   return (
-    <div>
-      <Layout>
-        <div className='empty-space'></div>
-        <div className='container'>
-          <div className='row'>
-            <div className='col border'>
-              <h1>advertising privacy policy...</h1>
+    <Layout>
+      <div className='empty-space'></div>
+      <section className='content-page'>
+        <div className='container-fluid'>
+          <div className='container'>
+            <div className='row pb-3'>
+              <div className='col'>
+                <h3>
+                  {data.pagePrivacyPolicyAdvertising.data.attributes.page_title}
+                </h3>
+              </div>
+            </div>
+
+            <div className='row'>
+              <div className='col'>
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      data.pagePrivacyPolicyAdvertising.data.attributes
+                        .page_content,
+                  }}
+                ></span>
+              </div>
             </div>
           </div>
         </div>
-      </Layout>
-    </div>
+      </section>
+    </Layout>
   );
 };
 
