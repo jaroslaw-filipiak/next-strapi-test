82e4d81a4913a16cca452e8092dad0ed8c29d4be
Jarosław Filipiak <info@j-filipiak.pl>
2022-07-08 10:25:30 +0200

    disable nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,6 @@
 import './../scss/oan.scss';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from '../lib/apollo';
-import React, { useEffect, useState } from 'react';
-
-import Router from 'next/router';
-import NProgress from 'nprogress'; //nprogress module
-import 'nprogress/nprogress.css'; //styles of nprogress
-
-NProgress.configure({
-  template: `<div class="bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>`,
-});
-
-//Binding events.
-Router.events.on('routeChangeStart', () => {
-  console.log('route change start');
-  NProgress.start();
-});
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
   return (
